5bdc28acac15f6f09626a5e94a1931cb78336c6c
amirizhar <amir_izhar@yahoo.com>
2023-01-24 23:19:31 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'book.centre',
+  appName: 'book-centre',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
