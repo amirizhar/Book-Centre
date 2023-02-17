ea57d37d20d73de9da002c500a7c2c6371c9c60a
amirizhar <amir_izhar@yahoo.com>
2023-02-17 23:37:30 +0800

    update-developer/appversion

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -19,11 +19,15 @@ const Tab3: React.FC = () => {
         </IonHeader>
         {/* <ExploreContainer name="Tab 4 page" /> */}
         <img src={header} alt="header"/>
+        <p><b>About The App</b></p>
         <p style={{textAlign: 'justify', margin: 25,}}>The best thing that will happen to a book worm or people who rely on books in their daily life is when they can get the correct information of books easily. With current book searching system that available on internet, sometimes it is hard for users to get information of a book due to incomplete information provided by the website.
         {'\n'}
         With the Online Book Centre, almost all information will be provided such as the book title, author name, summary of content and other information. Besides that, current available system on internet is also lack of interface design and not user friendly which somehow makes users feel not attracted to that website.
         In short, the purpose of this website is to create a better book searching system that has an attractive interface design which will facilitate users to obtain their requirements accordingly.
         </p>
+        <p><b>App Version: </b></p>
+
+        <p><b>Developer: </b></p>
       </IonContent>
     </IonPage>
   );
